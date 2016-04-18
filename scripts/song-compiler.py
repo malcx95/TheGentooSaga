8a1a12f3892e315c5aaf3f9f55d216fbbe5878dc
Emil Segerbäck <emilsegerback@gmail.com>
2016-04-18 12:36:13 +0200

    added test music

--- scripts/song-compiler.py
+++ scripts/song-compiler.py
@@ -1,25 +1,53 @@
-#!/usr/dev/env python3
+#!/usr/bin/env python3
+
+import sys
 
 offsets = {
-    'C' : -9,
-    'C#' : -8,
-    'Db' : -8,
-    'D' : -7,
-    'D#' : -6,
-    'Eb' : -6,
-    'E' : -5,
-    'F' : -4,
-    'F#' : -3,
-    'Gb' : -3,
-    'G' : -2,
-    'G#' : -1,
-    'Ab' : -1,
-    'A' : 0,
-    'A#' : 1,
-    'Bb' : 1,
-    'B' : 2
+    'C'  : 0,
+    'C#' : 1,
+    'Db' : 1,
+    'D'  : 2,
+    'D#' : 3,
+    'Eb' : 3,
+    'E'  : 4,
+    'F'  : 5,
+    'F#' : 6,
+    'Gb' : 6,
+    'G'  : 7,
+    'G#' : 8,
+    'Ab' : 8,
+    'A'  : 9,
+    'A#' : 10,
+    'Bb' : 10,
+    'B'  : 11
 }
 
-note_and_octave = input()
-octave = int(note_and_octave[-1])
-note = note_and_octave[:-1]
+def note_number(note_name):
+    octave = int(note_name[-1])
+    note = note_name[:-1]
+    return offsets[note] + octave*12
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
+
+def bit_string(val, nbits):
+    newc = bin(val)
+    bits = newc[2:]
+    return bits.zfill(nbits)
+
+# Calculation
+input_data = sys.stdin.read().split()
+chunkified_data = chunks(input_data, 2)
+parsed_data = [(int(length) - 1, note_number(name)) for length, name in chunkified_data]
+
+# Formatting
+bin_data = [bit_string(length, 2) + bit_string(number, 6) for length, number in parsed_data]
+int_data = [int(bits, 2) for bits in bin_data]
+hex_data = ['x"{:02x}"'.format(value) for value in int_data]
+
+# Output
+lines = [", ".join(chunk) for chunk in chunks(hex_data, 8)]
+output = ",\n".join(lines)
+print(output)
