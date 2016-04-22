e522f06589f7e39e3f910fc270a6dc0eda5680d1
Robin Sliwa <robsl733@student.liu.se>
2016-04-22 14:27:07 +0200

    fixed syntax errors in vga, main and data memory

--- vhdl/vga.vhd
+++ vhdl/vga.vhd
@@ -11,7 +11,7 @@ entity vga is
             vgaBlue      : out std_logic_vector(2 downto 1);
             Hsync, Vsync : out std_logic;
             tilePixel    : in std_logic_vector(7 downto 0); -- Tilepixel data
-            tileAddr     : out unsigned (12 downto 0); -- Tile adress
+            tileAddr     : out unsigned (12 downto 0) -- Tile adress
         );
 end vga;
 
