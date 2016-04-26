bc7ff60a47c78c5078c67dce2cdaad116e733262
Malcolm Vigren <malvi108@student.liu.se>
2016-04-26 13:37:20 +0200

    Fix robins shitty code

--- vhdl/program_memory.vhd
+++ vhdl/program_memory.vhd
@@ -12,43 +12,26 @@ end program_memory;
 architecture Behavioral of program_memory is
     constant nop : std_logic_vector(31 downto 0) := x"54000000";
     
-    type memory_type is array (0 to 27) of std_logic_vector(31 downto 0);
+    type memory_type is array (0 to 10) of std_logic_vector(31 downto 0);
     signal program_memory : memory_type := ( 
-	x"18200001",	--         MOVHI   R1,1        ; R1 = 1
-	x"18400004",	--         MOVHI   R2,4        ; R2 = 4
-	x"9C210001",	-- LOOP:   ADDI    R1,R1,0B01  ; INCEMENT R1 BY 1
-	x"03FFFFFF",	--         JMP     LOOP        ; JUMP BACK TO LOOP
-	x"E0611000",	--         ADD     R3,R1,R2    ; R3 = 5
-	x"9C420002",	--         ADDI    R2,R2,0B10     ; R2 = 6
-	x"9C210001",	--         ADDI    R1,R1,0B01     ; R1 = 2
-	x"84810014",	--         LW      R4,R1,20    ; R4 = 2
-	x"E0A11006",	--         MUL     R5,R1,R2    ; R5 = 12
-	x"54000000",	--         NOP                 ; 
-	x"18200001",	--         MOVHI   R1,1        ; R1 = 1
-	x"18400001",	--         MOVHI   R2,1        ; R2 = 1
-	x"E4011000",	--         SFEQ    R1,R2       ; F = 1
-	x"18200002",	--         MOVHI   R1,2        ; R1 = 2
-	x"54000000",	--         NOP                 ; 
-	x"54000000",	--         NOP                 ; 
-	x"E4011000",	--         SFEQ    R1,R2       ; F = 0
-	x"54000000",	--         NOP                 ; 
-	x"54000000",	--         NOP                 ; 
-	x"54000000",	--         NOP                 ; 
-	x"54000000",	--         NOP                 ; 
-	x"18200005",	--         MOVHI   R1,5        ; R1 = 5
-	x"E4230800",	--         SFNE    R3,R1       ; F = 1
-	x"54000000",	--         NOP                 ;
-	x"54000000",	--         NOP                 ;
-	x"54000000",	--         NOP                 ;
-	x"BC210005",	--         SFNEI   R1,5        ; F = 0
-	x"D4060800"		--         SW      R6,R1,0     ;
+	x"18000000",	-- 		MOVHI	R0, 0 # R0 BARA 0
+	x"9C20FFFF",	-- 		ADDI	R1, R0, 0XFFFF # R1 BARA 1
+	x"84408000",	-- LOOP:	LW		R2, R0, SPACE
+	x"E4001000",	-- 		SFEQ	R0, R2
+	x"13FFFFFE",	-- 		BF		LOOP
+	x"84408000",	-- BLINK:	LW		R2, R0, SPACE
+	x"D5000800",	-- 		SW		R0, R1, 0X4000
+	x"E4201000",	-- 		SFNE	R0, R2
+	x"13FFFFFD",	-- 		BF		BLINK
+	x"D5000000",	-- 		SW		R0, R0, 0X4000
+	x"03FFFFF8"		-- 		JMP		LOOP
  );
 
 begin
     process(clk)
     begin
         if (rising_edge(clk)) then
-            if (address <= 27) then
+            if (address <= 10) then
                 data <= program_memory(to_integer(address));
             else
                 data <= nop;
