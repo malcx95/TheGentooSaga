5407febe249645f02d6afbe64271579ee570a187
Malcolm Vigren <malvi108@student.liu.se>
2016-04-28 15:07:37 +0200

    Recompiled

--- vhdl/program_memory.vhd
+++ vhdl/program_memory.vhd
@@ -12,15 +12,9 @@ end program_memory;
 architecture Behavioral of program_memory is
     constant nop : std_logic_vector(31 downto 0) := x"54000000";
 
-    type memory_type is array (0 to 7) of std_logic_vector(31 downto 0);
+    type memory_type is array (0 to 1) of std_logic_vector(31 downto 0);
     signal program_memory : memory_type := (
-	x"54000000",	-- LOOP:		NOP
-	x"84208000",	-- 	LW		R1, R0, LEFT
-	x"E14A0800",	-- 			ADD		R10, R10, R1
-	x"84208001",	-- 			LW		R1, R0, RIGHT
-	x"E14A0802",	-- 			SUB		R10, R10, R1
 	x"D5005002",	-- 			SW		R0, R10, LED2
-	x"03FFFFFD",	-- 			JMP		LOOP
 	x"54000000"		-- 			NOP
  );
 
@@ -28,7 +22,7 @@ begin
     process(clk)
     begin
         if (rising_edge(clk)) then
-            if (address <= 7) then
+            if (address <= 1) then
                 data <= program_memory(to_integer(address));
             else
                 data <= nop;
