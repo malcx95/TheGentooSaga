04f77380ac8068c030b8937aefe669eee1566c3e
Malcolm Vigren <malvi108@student.liu.se>
2016-05-01 09:24:13 +0200

    Added test files

---
+++ assembler/hallon.s
@@ -0,0 +1,25 @@
+include shit
+
+const one:	1
+const zero:	0b0
+const rand: 0xfa
+
+func coolshit:
+HA:	
+nop
+	nop
+
+	add r1, r2, r4
+	nop
+	jmp HA
+	jmp L
+	lw	r3,r4, potato
+L:	NOP
+end
+
+func nope:
+
+	lw	r3,r4, potato
+	nop
+	add r1, r2, r3
+end
