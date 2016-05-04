4cb27a0e1f21a294452ae3129f24c41483b125b8
Robin Sliwa <robsl733@student.liu.se>
2016-05-04 18:57:26 +0200

    added script to convert json map to hex and added new tiles

---
+++ scripts/level_converter/convert_map.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python2
+
+import os
+import json
+
+save_path = '../../vhdl/'
+name_of_file = 'level_mem'
+completeName = os.path.join(save_path, name_of_file+".vhd")  
+open_file = open(completeName, 'w')
+tile = 0
+
+
+skeleton_top = """library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity level_mem is
+    port (
+        clk      : in std_logic;
+        data_out : out std_logic_vector(4 downto 0);
+        addr    : in unsigned(11 downto 0);
+        query_addr : in unsigned(11 downto 0);
+        query_result : out std_logic
+         );
+end level_mem;
+
+architecture Behavioral of level_mem is
+    type ram_t is array (0 to 2249) of std_logic_vector(4 downto 0);
+    signal pictMem : ram_t := (
+"""
+
+skeleton_bottom = """
+);
+
+signal query_help : std_logic_vector(5 downto 0);
+
+begin
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            data_out <= pictMem(to_integer(addr));
+            query_help <= pictMem(to_integer(query_addr));
+        end if;
+    end process;
+
+    query_result <= '0' when query_help > 15 else '1';
+
+end Behavioral;
+"""
+
+open_file.write(skeleton_top)
+with open('testmap.json') as data_file:
+    json_data = json.load(data_file)
+map_data = json_data["layers"][0]["data"]
+
+for number in map_data:
+    if (tile%5 == 0):
+        if (not tile == 0):
+            open_file.write("\n")
+        open_file.write("           ")
+    elif (tile == 2249):
+        open_file.write("{0:05b}".format(number-1))
+         
+    else:
+        open_file.write("{0:05b}".format(number-1)+", ")
+    tile+=1
+open_file.write(skeleton_bottom)
+print "File successfully written!"
+
